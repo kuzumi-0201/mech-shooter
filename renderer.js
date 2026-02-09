48c74e8727e8116406086ebc53cdb605e44337e4
kuzumi-0201 <kuzumi0201@gmail.com>
2026-02-09 17:49:18 +0900

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,3 +1,22 @@
 window.addEventListener("gamepadconnected", (e) => {
   alert("プロコン認識！");
 });
+let padIndex = null;
+
+window.addEventListener("gamepadconnected", (e) => {
+  padIndex = e.gamepad.index;
+  alert("プロコン認識！");
+  requestAnimationFrame(loop);
+});
+
+function loop() {
+  const pad = navigator.getGamepads()[padIndex];
+  if (pad) {
+    pad.buttons.forEach((b, i) => {
+      if (b.pressed) {
+        console.log("押されたボタン番号:", i);
+      }
+    });
+  }
+  requestAnimationFrame(loop);
+}
